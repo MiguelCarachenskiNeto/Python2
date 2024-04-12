ed47fe5d840582dd2c5b3abf1474167c222c879d
MiguelCarachenskiNeto <116725585+MiguelCarachenskiNeto@users.noreply.github.com>
2024-04-12 12:02:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,61 @@
 import os
 
+restaurante=[]
+
 def desligar_app():
-    os.system('cls')
-    print('Desligando o app\n')
+    os.system("clear")
+    print("Desligando o app\n")
+
+def opcao_invalida():
+    print("Opção inválida\n")
+    input("Digite uma tecla para voltar ao menu principal: ")
+    os.system("clear")
+    main()
+
+def cadastrar_novo_restaurante():
+    os.system("clear")
+    nome_do_restaurante=input("digite o nome do novo restaurante: \n")
+    restaurante.append(nome_do_restaurante)
+    os.system("clear")
+    print(f"O restaurante {nome_do_restaurante} foi cadastrado com sucesso!")
+    input("Digite uma tecla para voltar ao menu principal: ")
+    os.system("clear")
+    main()
 
 def nome_app():
-    print('Restaurante Expresso\n')
+    print("Restaurante Expresso\n")
 
-def lista():
-    print('1-Cadastrar um restaurante')
-    print('2-Listar restaurantes')
-    print('3-Ativar um restaurante')
-    print('4-Sair do programa\n')
+def exibir_opcoes():
+    print("1-Cadastrar um restaurante")
+    print("2-Listar restaurantes")
+    print("3-Ativar um restaurante")
+    print("4-Sair do programa\n")
 
-def opcoes():
-    escolha=int(input('Escolha uma opção: '))
-    print('Você selecionou a opção:',escolha,'\n')
-    if escolha == 1:
-        print('Você escolheu cadastrar um restaurante\n')
-    elif escolha == 2:
-        print('Você escolheu listar os restaurantes\n')
-    elif escolha == 3:
-        print('Você escolheu ativar um restaurante\n')
-    else:
-        print('Você escolheu sair do programa\n')
-        desligar_app()
+def escolher_opcoes():
+    try:
+        escolha=int(input("Escolha uma opção: "))
+        print("Você selecionou a opção:",escolha,"\n")
+        if escolha == 1:
+            print("Você escolheu cadastrar um restaurante\n")
+            cadastrar_novo_restaurante()
+        elif escolha == 2:
+            print("Você escolheu listar os restaurantes\n")
+        elif escolha == 3:
+            print("Você escolheu ativar um restaurante\n")
+        elif escolha == 4:
+            print("Você escolheu sair do programa\n")
+            desligar_app()
+        else:
+            opcao_invalida()
+
+    except:
+        opcao_invalida()
 
 def main():
+    os.system("clear")
     nome_app()
-    lista()
-    opcoes()
+    exibir_opcoes()
+    escolher_opcoes()
 
-if __name__=='__main__':
+if __name__=="__main__":
     main()
